c9391aa4b70d379259ffedbb19bb1b2a74ae9aff
Joyce Tao <jtao@lcs.on.ca>
2021-11-30 15:31:42 -0500

    Created a slider to change/input the value of celsius degree; created an output of the value (text) below the slider

--- TemperatureConverter/ContentView.swift
+++ TemperatureConverter/ContentView.swift
@@ -8,14 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    // MARK: Stored properties
+    @State var celciusDegree: Double = 20.0
+    
+    // MARK: Computed properties
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(alignment: .leading, spacing: 10) {
+            // Celcius degree input
+            Text("Celcius")
+                .font(.title2)
+                .bold()
+            
+            Slider(value: $celciusDegree,
+                   in: -50.0...50.0,
+                   step: 1.0,
+                   label: {
+                Text("Celcius")
+            }, minimumValueLabel: {
+                Text("-50")
+            },
+                   maximumValueLabel: {
+                Text("50")
+            })
+            
+            // Celcius degree output
+            HStack {
+                Spacer()
+                Text("\(String(format: "%.0f", celciusDegree))°C")
+                    .bold()
+                    .font(.title2)
+                Spacer()
+            }
+            
+            Spacer()
+        }
+        .padding()
+        .navigationTitle("Temperature Converter")
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        NavigationView {
+            ContentView()
+        }
     }
 }
